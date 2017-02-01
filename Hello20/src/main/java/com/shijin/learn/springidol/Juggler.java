37f11c12ff785e7b0aabbf2bb233b2432c847d93
jeri <shijin2008@gmail.com>
2017-02-01 18:50:40 +0800

    implement Spring context

---
+++ Hello20/src/main/java/com/shijin/learn/springidol/Juggler.java
@@ -0,0 +1,44 @@
+package com.shijin.learn.springidol;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Juggler implements Performer {
+  private static final Logger logger = LogManager.getLogger(Performer.class);
+
+  private int beanBags = 3;
+
+  public Juggler() {}
+
+  public Juggler(int beanBags) {
+    this.beanBags = beanBags;
+  }
+
+  @Override
+  public void perform() throws PerformanceException {
+    logger.debug("performer start...");
+    System.out.println("JUGGLING " + beanBags + " BEANBAGS");
+    logger.debug("performer finished...");
+  }
+
+  public static void main(String[] args) {
+    logger.info("main start...");
+    
+    @SuppressWarnings("resource")
+    ApplicationContext ctx =
+        new ClassPathXmlApplicationContext("com/shijin/learn/springidol/spring-idol.xml");
+
+    try{
+      Performer performer = (Performer) ctx.getBean("duke");
+      performer.perform();     
+    }catch(Exception e){
+      logger.error(e.getMessage());
+    }
+
+    logger.info("main end.");
+
+  }
+
+}
---
+++ Hello20/src/main/java/com/shijin/learn/springidol/PerformanceException.java
@@ -0,0 +1,6 @@
+package com.shijin.learn.springidol;
+
+public class PerformanceException extends Exception {
+  private static final long serialVersionUID = 727644316428269197L;
+
+}
